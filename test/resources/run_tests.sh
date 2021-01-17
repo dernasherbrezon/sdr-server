864719399d9763a7e2350601ec70c2d2d4fc6c51
dernasherbrezon <rodionovamp@mail.ru>
2021-01-17 22:32:02 +0000

    trying to fix segmentation fault in libvolk

--- test/resources/run_tests.sh
+++ test/resources/run_tests.sh
@@ -5,6 +5,7 @@ set +e
 EXIT_CODE=0
 for file in test_*; do
 	[[ ${file} == *.dSYM ]] && continue
+	export VOLK_GENERIC=1
 	valgrind -v --error-exitcode=1 -q --tool=memcheck --leak-check=yes --show-reachable=yes ./${file}
 	CURRENT_EXIT_CODE=$?
 	if [ ${CURRENT_EXIT_CODE} != 0 ]; then

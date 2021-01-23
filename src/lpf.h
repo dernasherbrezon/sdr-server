1f6df81485d4a41da8ed386326c2b8d85a380637
dernasherbrezon <rodionovamp@mail.ru>
2021-01-23 13:50:44 +0000

    fixing the build

--- src/lpf.h
+++ src/lpf.h
@@ -1,6 +1,8 @@
 #ifndef SRC_LPF_H_
 #define SRC_LPF_H_
 
+#include <stdint.h>
+
 int create_low_pass_filter(float gain, uint32_t sampling_freq, uint32_t cutoff_freq, uint32_t transition_width, float **taps, size_t *len);
 
 #endif /* SRC_LPF_H_ */

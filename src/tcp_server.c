f9ea0b41b958763511901a59ab01f6d47950ff75
dernasherbrezon <rodionovamp@mail.ru>
2021-01-31 11:50:40 +0000

    remove dead code

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -96,10 +96,6 @@ int validate_client_config(struct client_config *config, struct server_config *s
 		fprintf(stderr, "missing band_freq parameter\n");
 		return -1;
 	}
-	if (config->sampling_rate > server_config->band_sampling_rate) {
-		fprintf(stderr, "requested sampling rate is less than on server: %u\n", server_config->band_sampling_rate);
-		return -1;
-	}
 	uint32_t requested_min_freq = config->center_freq - config->sampling_rate / 2;
 	uint32_t server_min_freq = config->band_freq - server_config->band_sampling_rate / 2;
 	if (requested_min_freq < server_min_freq) {
